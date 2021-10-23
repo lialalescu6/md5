1d95af8ee43dc25b0f20ac000165b6f098c6367e
lialalescu <lia.lalescu6@gmail.com>
2021-10-23 12:43:39 +0300

    add a-z strings

---
+++ generator.py
@@ -0,0 +1,9 @@
+alp = 'abcdefghijklmnopqrstuvwxyz'
+for a in alp:
+    for b in alp:
+        for c in alp:
+            for d in alp:
+                for e in alp:
+                    for f in alp:
+                        st = a+b+c+d+e+f
+                        print(st)
